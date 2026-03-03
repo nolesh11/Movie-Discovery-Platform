fb70a23c97e7eb11ee46b963b612492037bc5087
nolesh11 <leshkanogay@gmail.com>
2026-03-04 00:27:49 +0500

    added link for movie details

--- src/js/components/Search.js
+++ src/js/components/Search.js
@@ -1,65 +1,63 @@
 import { getMoviespages } from "../api/api.js";
 
 export async function Search(header) {
-  const movies = await getMoviespages(5);
-  const btn = header.querySelector('.search-btn');
-  
+  const movies = await getMoviespages(44);
+  const btn = header.querySelector(".search-btn");
+
   function searchPanel() {
-    const existing = header.querySelector('.search-container')
-    const input = header.querySelector('input')
+    const existing = header.querySelector(".search-container");
+    const input = header.querySelector("input");
 
-    if(existing) {
+    if (existing) {
       existing.remove();
-      input?.classList.remove('open')
+      input?.classList.remove("open");
       return;
-    } 
+    }
 
-    input?.classList.add('open')
+    input?.classList.add("open");
 
-    const searchContainer = document.createElement('div');
-    searchContainer.className = 'search-container';
+    const searchContainer = document.createElement("div");
+    searchContainer.className = "search-container";
 
-    // searchContainer.innerHTML = ``;
-    
-    // const cont = searchContainer.querySelector('.search-results')
-    
-    input.addEventListener('input', (e) => {
+    input.addEventListener("input", (e) => {
       const query = e.target.value.trim().toLowerCase();
-      
-      if(!query) {
-        return cont.innerHTML = '';
+
+      if (!query) {
+        return (searchContainer.innerHTML = "");
       }
 
-      const result = movies.filter(m => {
-        const title = (m.title || '').toLowerCase();
-        return title.includes(query)
-      })
+      const result = movies.filter((m) => {
+        const title = (m.title || "").toLowerCase();
+        return title.includes(query);
+      });
 
-      render(result)
-      
+      render(result);
     });
 
     function render(movies) {
       searchContainer.innerHTML = movies
         .slice(0, 10)
-        .map(m => `
-          <div class='search-results'>
-            <div class='result-img'>
-              <img src='https://image.tmdb.org/t/p/w300${m.poster_path}' />
+        .map(
+          (m) => `
+          <a href="#/movie/${m.id}">
+            <div class='search-results'>
+              <div class='result-img'>
+                <img src='https://image.tmdb.org/t/p/w300${m.poster_path}' />
+              </div>
+              <div class='result-info'>
+                <h3>${m.title}</h3>
+                <p>Realse date: ${m.release_date}</p>
+              </div>
             </div>
-            <div class='result-info'>
-              <h3>${m.title}</h3>
-              <p>Realse date: ${m.release_date}</p>
-            </div>
-          </div>
-          `
+          </a>
+          
+          `,
         )
-        .join('')
+        .join("");
     }
-    
-    header.append(searchContainer)
+
+    header.append(searchContainer);
   }
 
-  btn.addEventListener('click', searchPanel)
-
+  btn.addEventListener("click", searchPanel);
 }
